04fe36307ba488baa99fc62031a254a38083e33b
Nastya <nastya.syasyafi@gmail.com>
2024-09-29 11:30:04 +0300

    Update ImageCard.jsx

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,8 +1,11 @@
 export default function ImageCard({ image, onImageClick }) {
-  //    const image={image.urls.full};
   return (
-    <div onClick={() => onImageClick(image)}>
-      <img src={image.urls.small} alt={image.alt_description} />
+    <div>
+      <img
+        src={image.urls.small}
+        alt={image.alt_description}
+        onClick={() => onImageClick(image)}
+      />
     </div>
   );
 }
